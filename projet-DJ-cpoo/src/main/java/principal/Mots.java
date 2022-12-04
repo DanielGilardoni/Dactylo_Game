4800d53e53b568933ac5c9041a73dd4eeb5286fa
jadecrtl <jadecortial@gmail.com>
2022-12-04 19:34:12 +0100

    test minime de base sur la classe mots pour tester mvn

--- projet-DJ-cpoo/src/main/java/principal/Mots.java
+++ projet-DJ-cpoo/src/main/java/principal/Mots.java
@@ -1,5 +1,13 @@
 package principal;
 
 public class Mots {
-    
+    private String texte;
+    public Mots(String texte) {
+        if (texte == null) {
+            throw new IllegalArgumentException("Il doit y avoir du texte pour le logiciel!!");
+        }
+        else {
+            this.texte = texte;
+        }
+    }
 }
--- projet-DJ-cpoo/src/test/java/principal/MotsTest.java
+++ projet-DJ-cpoo/src/test/java/principal/MotsTest.java
@@ -1,5 +1,14 @@
 package principal;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+//import static org.assertj.core.api.Assertions.assertThat;
+
 public class MotsTest {
-    
+
+    @Test
+    public void creationDuTexte(){
+        assertThatThrownBy(() -> new Mots(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
