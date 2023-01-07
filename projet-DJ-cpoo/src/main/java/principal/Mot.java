0063a40e7b3ec9b37674581da38f8ae6596ea42f
Daniel Gilardoni <dany.gilardoni@gmail.com>
2023-01-07 14:26:36 +0100

    Modification et ajout de fonctions pour compter les caractéres utiles et ajouts de tests

--- projet-DJ-cpoo/src/main/java/principal/Mot.java
+++ projet-DJ-cpoo/src/main/java/principal/Mot.java
@@ -25,25 +25,34 @@ public class Mot {
         return this.mot;
     }
 
-    public static int compareMots(Mot a, Mot b){ //compareMots(attendu, tapé)
-        int countError = 0;
-        String motA = a.getMot();
-        String motB = b.getMot();
-        int sizeA = motA.length();
-        int sizeB = motB.length();
-        int sizeMin = 0;
+    // public static int compareMots(Mot a, Mot b){ //compareMots(attendu, tapé)
+    //     int countError = 0;
+    //     String motA = a.getMot();
+    //     String motB = b.getMot();
+    //     int sizeA = motA.length();
+    //     int sizeB = motB.length();
+    //     int sizeMin = 0;
 
-        if(sizeA < sizeB)
-            sizeMin = sizeA;
-        else 
-            sizeMin = sizeB;
-        for(int i=0;i<sizeMin;i++){//On va compter le nombres de caractéres différents
-            if(motA.charAt(i) != motB.charAt(i))
-                countError += 1;
-        } 
-        countError += Math.abs((sizeA - sizeB));//Il y a |sizeA - sizeB| caractéres non tapés ou tapés en trop 
+    //     if(sizeA < sizeB)
+    //         sizeMin = sizeA;
+    //     else 
+    //         sizeMin = sizeB;
+    //     for(int i=0;i<sizeMin;i++){//On va compter le nombres de caractéres différents
+    //         if(motA.charAt(i) != motB.charAt(i))
+    //             countError += 1;
+    //     } 
+    //     countError += Math.abs((sizeA - sizeB));//Il y a |sizeA - sizeB| caractéres non tapés ou tapés en trop 
 
-        return countError;
+    //     return countError;
+    // }
+
+    public boolean compareMots(Mot a, Mot b){
+        return a.getMot().equals(b.getMot());
+    }
+
+    public int countUtils(Mot a, int nbCharTyped){//A appeler si le mot tapé est valide
+        int size = a.getMot().length();
+        return (2*size - nbCharTyped);//la taille du mot - le nombre de char qu'on aura tapé puis effacé est le nombre de char utiles
     }
 
     public static class Builder {
